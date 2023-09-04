8f72f1f8c15c63dea7d8eee0e8a626b521484b37
Zhengqian Kuang <dev.kuang@gmail.com>
2023-09-04 16:47:38 -0600

    monorepo

--- app/packages/app2/App.tsx
+++ app/packages/app2/App.tsx
@@ -77,8 +77,8 @@ function App(): JSX.Element {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+            Edit <Text style={styles.highlight}>packages/app2/App.tsx</Text> to
+            change this screen and then come back to see your edits.
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
